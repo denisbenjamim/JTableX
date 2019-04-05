8e998515e3ccd443d03d03fb965dda75c941aa0d
denis.benjamim <denis.benjamim@gmail.com>
2019-04-05 18:17:49 -0300

    Adicionado DateModel para componente

--- src/br/com/blogspot/denisbenjamim/componentes/editoresrenderizadores/JSpinnerCellEditorRendererTable.java
+++ src/br/com/blogspot/denisbenjamim/componentes/editoresrenderizadores/JSpinnerCellEditorRendererTable.java
@@ -3,6 +3,7 @@ package br.com.blogspot.denisbenjamim.componentes.editoresrenderizadores;
 import java.awt.Component;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.SpinnerDateModel;
 import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
@@ -26,13 +27,19 @@ public class JSpinnerCellEditorRendererTable extends AbstractCellEditorRendererT
         this.spinner = new JSpinner(new SpinnerNumberModel(init, min, max, 1));
         this.spinner.addChangeListener(this);
     }
-
+    
     public JSpinnerCellEditorRendererTable(Object[] objetos) {
         this.spinner = new JSpinner(new SpinnerListModel(objetos));
     }
 
     public JSpinnerCellEditorRendererTable setNumberModel(int init, int min, int max) {
         this.spinner.setModel(new SpinnerNumberModel(init, min, max, 1));
+        this.spinner.setValue(init);
+        return this;
+    }
+    public JSpinnerCellEditorRendererTable setDateModel(String padraoFormatacao) {
+        this.spinner.setModel(new SpinnerDateModel());
+        this.spinner.setEditor(new JSpinner.DateEditor(this.spinner, padraoFormatacao));
         return this;
     }
 
@@ -44,21 +51,21 @@ public class JSpinnerCellEditorRendererTable extends AbstractCellEditorRendererT
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         if (value != null && value instanceof Integer) {
-            this.spinner.setValue(value);
-        }
+            this.spinner.setValue(value);            
+        }        
         return this.spinner;
     }
 
     @Override
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         if (value != null) {
-            this.spinner.setValue(value);
+            this.spinner.setValue(value);            
         }
         return this.spinner;
     }
 
     @Override
-    public Component getComponent() {
+    public JSpinner getComponent() {
         return this.spinner;
     }
 
